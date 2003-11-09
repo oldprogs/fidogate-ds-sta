41c497f02619feb53b72b43eb9987bd767eabcb1
rusfidogate <rusfidogate>
2003-11-09 16:18:59 +0000

    See ChangeLog

--- version.h
+++ version.h
@@ -2,7 +2,7 @@
 /*****************************************************************************
  * FIDOGATE --- Gateway software UNIX Mail/News <-> FIDO NetMail/EchoMail
  *
- * $Id: version.h,v 1.1 2003/11/05 00:37:10 rusfidogate Exp $
+ * $Id: version.h,v 1.2 2003/11/09 16:18:59 rusfidogate Exp $
  *
  * Global version number, patch level
  *****************************************************************************/
@@ -10,6 +10,6 @@
 #define VERSION_MAJOR	5
 #define VERSION_MINOR	1
 #define PATCHLEVEL	0
-#define EXTRAVERSION	"ds-rc1"
+#define EXTRAVERSION	"ds"
 
 #define STATE		"stable"
