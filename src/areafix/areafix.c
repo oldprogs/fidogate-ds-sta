a6b8a0532ea3c615b38da27787364c5ee569b874
rusfidogate <rusfidogate>
2004-03-24 18:47:19 +0000

    See ChangeLog

--- src/areafix/areafix.c
+++ src/areafix/areafix.c
@@ -2,7 +2,7 @@
 /*****************************************************************************
  * FIDOGATE --- Gateway UNIX Mail/News <-> FTN NetMail/EchoMail
  *
- * $Id: areafix.c,v 1.12 2004/03/24 06:03:03 rusfidogate Exp $
+ * $Id: areafix.c,v 1.13 2004/03/24 18:47:19 rusfidogate Exp $
  *
  * Common Areafix functions
  *
@@ -1228,7 +1228,7 @@ int cmd_listall(Node *node)
 #endif /* !FTN_ACL */
 #ifdef AFSEND_ECHO_STATUS
 	    sprintf(tmp, "   '%s' %s",p->state, ctime(&p->time));
-	    tmp[strlen(tmp)-1] = NULL;
+	    tmp[strlen(tmp)-1] = 0;
 	    BUF_APPEND(buffer, tmp);
 #endif /* AFSEND_ECHO_STATUS */
 	    areafix_printf("%s",buffer);
@@ -1474,7 +1474,7 @@ int cmd_list(Node *node, int flag)	/* FALSE -> %avail; TRUE -> %list */
 #endif /* !FTN_ACL */
 #ifdef AFSEND_ECHO_STATUS
 		sprintf(tmp, "   '%s' %s",p->state, ctime(&p->time));
-		tmp[strlen(tmp)-1] = NULL;
+		tmp[strlen(tmp)-1] = 0;
 		BUF_APPEND(buffer, tmp);
 #endif /* AFSEND_ECHO_STATUS */
 		areafix_printf("%s",buffer);
@@ -1851,7 +1851,7 @@ int cmd_sub(Node *node, char *area_in, Textlist *upl)
 #endif /* ANSWER_OK */
 #ifdef AFSEND_ECHO_STATUS
 		    sprintf(tmp, " Stat: '%s' last msg: %s",p->state, ctime(&p->time));
-		    tmp[strlen(tmp)-1] = NULL;
+		    tmp[strlen(tmp)-1] = 0;
 		    BUF_APPEND(buffer, tmp);
 #endif /* AFSEND_ECHO_STATUS */
 		    areafix_printf("%s",buffer);
