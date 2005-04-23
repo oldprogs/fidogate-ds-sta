64ff4a6d4eb08757dfe6ba292a96e4fb791cee4b
anray <anray>
2005-04-23 15:02:21 +0000

    ======= Release 5.1.3ds ======================== ------- CVS tag R513 --------------------------- * version.h (EXTRAVERSION): Bump to 5.1.3ds. * configure.in: Ditto. * doc/FAQ.ru: Now this is a confiuration mini FAQ (part 10 of FAQ_Config.ru). * doc/FAQ_Config.ru: Added full fidogate russian configuration FAQ. * doc/README.ru: Update.

--- version.h
+++ version.h
@@ -2,14 +2,14 @@
 /*****************************************************************************
  * FIDOGATE --- Gateway software UNIX Mail/News <-> FIDO NetMail/EchoMail
  *
- * $Id: version.h,v 5.1 2004/12/10 19:09:48 anray Exp $
+ * $Id: version.h,v 5.2 2005/04/23 15:02:22 anray Exp $
  *
  * Global version number, patch level
  *****************************************************************************/
 
 #define VERSION_MAJOR	5
 #define VERSION_MINOR	1
-#define PATCHLEVEL	2
+#define PATCHLEVEL	3
 #define EXTRAVERSION	"ds"
 
 #define STATE		"stable"
