ea62488a0c1751995bdbb97f1f023e7088544bb0
anray <anray>
2006-03-22 21:14:29 +0000

    See ChangeLog

--- version.h
+++ version.h
@@ -2,14 +2,14 @@
 /*****************************************************************************
  * FIDOGATE --- Gateway software UNIX Mail/News <-> FIDO NetMail/EchoMail
  *
- * $Id: version.h,v 5.2 2005/04/23 15:02:22 anray Exp $
+ * $Id: version.h,v 5.3 2006/03/22 21:14:29 anray Exp $
  *
  * Global version number, patch level
  *****************************************************************************/
 
 #define VERSION_MAJOR	5
 #define VERSION_MINOR	1
-#define PATCHLEVEL	3
+#define PATCHLEVEL	4
 #define EXTRAVERSION	"ds"
 
 #define STATE		"stable"
