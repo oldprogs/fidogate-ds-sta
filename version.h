d16ecd5e76ebe7c0b529557bec564b147e29179d
anray <anray>
2006-10-14 09:04:13 +0000

    ======= Release 5.1.5ds ======================== ------- CVS tag R515 --------------------------- * configure.in: Bump to 5.1.5ds. * version.h (PATCHLEVEL): Ditto.

--- version.h
+++ version.h
@@ -2,14 +2,14 @@
 /*****************************************************************************
  * FIDOGATE --- Gateway software UNIX Mail/News <-> FIDO NetMail/EchoMail
  *
- * $Id: version.h,v 5.3 2006/03/22 21:14:29 anray Exp $
+ * $Id: version.h,v 5.4 2006/10/14 09:04:13 anray Exp $
  *
  * Global version number, patch level
  *****************************************************************************/
 
 #define VERSION_MAJOR	5
 #define VERSION_MINOR	1
-#define PATCHLEVEL	4
+#define PATCHLEVEL	5
 #define EXTRAVERSION	"ds"
 
 #define STATE		"stable"
