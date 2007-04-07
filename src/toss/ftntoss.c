7c0aa61a6bcc378f0d5259572f00186cb6588f34
anray <anray>
2007-04-07 20:08:23 +0000

    See ChangeLog

--- src/toss/ftntoss.c
+++ src/toss/ftntoss.c
@@ -2,7 +2,7 @@
 /*****************************************************************************
  * FIDOGATE --- Gateway UNIX Mail/News <-> FIDO NetMail/EchoMail
  *
- * $Id: ftntoss.c,v 5.8 2007/01/29 10:52:22 anray Exp $
+ * $Id: ftntoss.c,v 5.9 2007/04/07 20:08:23 anray Exp $
  *
  * Toss FTN NetMail/EchoMail
  *
@@ -40,7 +40,7 @@
 
 
 #define PROGRAM 	"ftntoss"
-#define VERSION 	"$Revision: 5.8 $"
+#define VERSION 	"$Revision: 5.9 $"
 #define CONFIG		DEFAULT_CONFIG_MAIN
 
 
@@ -814,7 +814,7 @@ int do_unknown_area (char *areaname, Message *msg, MsgBody *body) {
 
     fglog("unknown area %s from %s", areaname, znfp1(&msg->node_from) );
     msgs_unknown++;
-    if(kill_unknown)
+    if(!kill_unknown)
 	return do_bad_msg(msg, body);
     return OK;
 }
